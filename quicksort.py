24e22fb4cad929261d970a15b4da06e652914c0d
Junyi Chai <phychai.junyi@gmail.com>
2016-11-13 16:21:00 -0500

    Implemented original and classical quick sort algo

--- quicksort.py
+++ quicksort.py
@@ -1,5 +1,5 @@
 def quick_sort(array, start=0, end=None):
-    """Sort in ascending order"""
+    """Sort in ascending order: my partion method"""
     if end is None:
         end = len(array) - 1
     if end-start <= 0:
@@ -19,4 +19,44 @@ def quick_sort(array, start=0, end=None):
         left += 1
     quick_sort(array, start, left-1)
     quick_sort(array, left, end)
-    return array
+
+
+def quick_sort_classic(array, start=0, end=None):
+    """Sort in ascending order: Cormen's partition method"""
+    if end is None:
+        end = len(array) - 1
+    if end-start <= 0:
+        return
+    pivot_val = array[end]
+    j = start
+    for i in range(start, end):
+        if array[i] <= pivot_val:
+            array[i], array[j] = array[j], array[i]
+            j += 1
+    array[end], array[j] = array[j], array[end]
+    quick_sort_classic(array, start, j-1)
+    quick_sort_classic(array, j+1, end)
+
+def quick_sort_original(array, start=0, end=None):
+    """Sort in ascending order: Hoare's method"""
+    if end is None:
+        end = len(array) - 1
+    if end-start <= 0:
+        return
+    pivot_val = array[start]
+    left = start + 1
+    right = end
+    while right > left:
+        while left < end and array[left] <= pivot_val:
+            left += 1
+        while right > left and array[right] > pivot_val:
+            right -= 1
+        array[left], array[right] = array[right], array[left]
+    if array[left] < pivot_val:
+        array[left], array[start] = array[start], array[left]
+    quick_sort_original(array, start, left-1)
+    quick_sort_original(array, left, end)
+
+a = [4,3,2,1,2,3,5,1,2]
+quick_sort_original(a)
+print a
--- test_quick_sort.py
+++ test_quick_sort.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from quicksort import quick_sort
+from quicksort import quick_sort, quick_sort_classic, quick_sort_original
 import random
 
 class TestQuick_sort(TestCase):
@@ -14,11 +14,49 @@ class TestQuick_sort(TestCase):
             [0, 1, 2, 2, 3, 3, 3, 4, 5, 6, 6, 7, 11, 13, 15])
 
     def test_quick_sort_2(self):
-        N = 1000
+        import time
+        N = 10000
         random.seed(0)
         A = []
         for i in range(N):
-            A.append(int(random.random()*10000))
+            A.append(int(random.random()*N*10))
         A_copy = list(A)
+        t0 = time.time()
         quick_sort(A)
-        self.assert_list_equal(A, sorted(A_copy))
+        t1 = time.time()
+        A_sorted = sorted(A_copy)
+        t2 = time.time()
+        print "My algo takes %f s vs native %f s with ratio %f" %(t1-t0, t2-t1, (t1-t0)/(t2-t1))
+        self.assert_list_equal(A, A_sorted)
+
+    def test_quick_sort_classic(self):
+        import time
+        N = 10000
+        random.seed(0)
+        A = []
+        for i in range(N):
+            A.append(int(random.random()*N*10))
+        A_copy = list(A)
+        t0 = time.time()
+        quick_sort_classic(A)
+        t1 = time.time()
+        A_sorted = sorted(A_copy)
+        t2 = time.time()
+        print "Classic algo takes %f s vs native %f s with ratio %f" %(t1-t0, t2-t1, (t1-t0)/(t2-t1))
+        self.assert_list_equal(A, A_sorted)
+
+    def test_quick_sort_orignal(self):
+        import time
+        N = 10000
+        random.seed(0)
+        A = []
+        for i in range(N):
+            A.append(int(random.random()*N*10))
+        A_copy = list(A)
+        t0 = time.time()
+        quick_sort_original(A)
+        t1 = time.time()
+        A_sorted = sorted(A_copy)
+        t2 = time.time()
+        print "Original algo takes %f s vs native %f s with ratio %f" %(t1-t0, t2-t1, (t1-t0)/(t2-t1))
+        self.assert_list_equal(A, A_sorted)
